428eb22b1113eca49a82edc519d6c40eddff6a8d
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 16:20:03 -0400

    aa

--- playground/rl_agent_prod/keras_model_cpp_2.py
+++ playground/rl_agent_prod/keras_model_cpp_2.py
@@ -5,8 +5,8 @@ class Foo(object):
     def __init__(self):
         self.obj = lib.KerasModel_new()
 
-    def init(self):
-        lib.KerasModel_init(self.obj)
+    def init(self, model_path):
+        lib.KerasModel_init(self.obj, model_path)
 
 if __name__ == "__main__":
     foo = Foo()
