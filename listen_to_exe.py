882867020f4ce7288799c034c82c14f6c05e670c
addyvan <avandenhoeve14@ubishops.ca>
2019-08-29 22:34:21 -0400

    pls

--- listen_to_exe.py
+++ listen_to_exe.py
@@ -1,12 +1,31 @@
 import sys
 from subprocess import Popen, PIPE,TimeoutExpired
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def parse_results(arr, data):
+    predictions = []
+    for char in data:
+        if is_number(char):
+            predictions.append(int(char)) 
+    return predictions
+
 def _read_output(proc):
     return_code = proc.poll()
     out, err = out_line, err_line = "",""
     while proc.poll() == None:
         out_line = proc.stdout.readline().decode('utf-8')
-        print(out_line)
+        if out_line[0:7] == "outline":
+            data = out_line[8:]
+            predictions = parse_results(data)
+
+    print("predictions: ", predictions)
+
 
     return ( out, err )
 
