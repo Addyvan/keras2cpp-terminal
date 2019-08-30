cb01745d1f2987f61b9dc5345a9f7105102f98f8
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 10:37:49 -0400

    a

--- playground/run_game.py
+++ playground/run_game.py
@@ -4,14 +4,6 @@ import sys
 
 def run_game():
     print("Running game")
-    # This is only for windows
-    if "run.ps1" not in algo1:
-        trailing_char = "" if algo1.endswith("\\") else "\\"
-        algo1 = algo1 + trailing_char + "run.ps1"
-    if "run.ps1" not in algo2:
-        trailing_char = "" if algo2.endswith("\\") else "\\"
-        algo2 = algo2 + trailing_char + "run.ps1"
-
     algo1_path = os.path.abspath("./rl_agent_prod/run.sh")
     algo2_path = os.path.abspath("./rl_agent_prod/run.sh")
 
