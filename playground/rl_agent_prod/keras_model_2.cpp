d984311b110ca9425d50886829758f5b9839918e
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 16:11:19 -0400

    aa

--- playground/rl_agent_prod/keras_model_2.cpp
+++ playground/rl_agent_prod/keras_model_2.cpp
@@ -9,16 +9,16 @@ class KerasModel {
     auto model;
     public:
 
-        void init(string model_path) {
-            model = fdeep::load_model(argv[1]);
+        void init(std::string model_path) {
+            model = fdeep::load_model(model_path);
         }
 
-        
+
     
     
 };
 
 extern "C" {
     KerasModel* KerasModel_new(){ return new KerasModel(); }
-    void KerasModel_init(KerasModel* kerasmodel, string model_path){ kerasmodel->init(model_path); }
+    void KerasModel_init(KerasModel* kerasmodel, std::string model_path){ kerasmodel->init(model_path); }
 }
