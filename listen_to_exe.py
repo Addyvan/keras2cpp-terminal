ec21ffca65eb2f2ac245dcae6b517cef68db276b
addyvan <avandenhoeve14@ubishops.ca>
2019-08-29 22:25:38 -0400

    Adding files

---
+++ listen_to_exe.py
@@ -0,0 +1,21 @@
+from subprocess import Popen, PIPE,TimeoutExpired
+
+def _read_output(self, proc):
+    return_code = proc.poll()
+    out, err = out_line, err_line = "",""
+    while proc.poll() == None:
+        out_line = proc.stdout.readline().decode('utf-8')
+        print(out_line)
+
+    return ( out, err )
+
+process_command = "./main"
+proc = Popen(process_command, shell=True, stdout=PIPE, stderr=sys.stderr, stdin=PIPE)
+
+try:
+    while self.proc.returncode == None and not self.chill:
+        outs , errs = self._read_output(self.proc)
+except TimeoutExpired:
+    print("TimeoutExpired: \n")
+    outs, errs = proc.communicate()
+    print("OUTS: ", outs, "ERRS: ", errs)
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
   std::cout<<"fun";
-  const auto model = fdeep::load_model("./output/fdeep_model.json");
+  //const auto model = fdeep::load_model("./output/fdeep_model.json");
   //const auto result = model.predict({fdeep::tensor5(fdeep::shape5(1, 1, 1, 1, 4), {1, 2, 3, 4})});
   //std::cout << fdeep::show_tensor5s(result) << std::endl;
 }
