e0cc606d4450b4c046b5e2e8aa4e90c8afc4d55e
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 12:03:44 -0400

    aaa

--- ping.cpp
+++ ping.cpp
@@ -8,9 +8,6 @@ int main()
     const auto model = fdeep::load_model("./fdeep_ping.json");
 
     fdeep::tensor5 input_data(fdeep::shape5(1, 1, 1, 420, 6), 0);
-    input_data.set(0, 0, 0, 0, 0, 1);
-    input_data.set(0, 0, 1, 0, 0, 2);
-    input_data.set(0, 0, 2, 0, 0, 3);
 
     for (int i = 0; i < 420; i++) {
         for (int j = 0; j < 6; j++) {
