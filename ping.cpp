edae225dc78aca4f13260d748dcfab38f30b4a58
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 11:07:31 -0400

    aa

--- ping.cpp
+++ ping.cpp
@@ -6,7 +6,7 @@
 int main()
 {
   const auto model = fdeep::load_model("./output/fdeep_model.json");
-  std::cout<< model.get_input_shapes() <<std::endl;
+  //std::cout<< model.get_input_shapes() <<std::endl;
   //const auto result = model.predict({fdeep::tensor5(fdeep::shape5(1, 1, 1, 1, 4), {1, 2, 3, 4})});
   //std::cout << fdeep::show_tensor5s(result) << std::endl;
   return 0;
