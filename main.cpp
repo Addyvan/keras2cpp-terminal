695f673314a2aee1d0fb17e73f726d3a1bc745a6
addyvan <avandenhoeve14@ubishops.ca>
2019-08-29 22:20:17 -0400

    Adding test files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fdeep/fdeep.hpp>
+
+int main()
+{
+  std::cout<<"fun";
+  const auto model = fdeep::load_model("./output/fdeep_model.json");
+  //const auto result = model.predict({fdeep::tensor5(fdeep::shape5(1, 1, 1, 1, 4), {1, 2, 3, 4})});
+  //std::cout << fdeep::show_tensor5s(result) << std::endl;
+}
