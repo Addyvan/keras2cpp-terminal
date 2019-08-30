1187b1cc18c0205e76b47391c6ef0cc904c4f780
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 13:04:05 -0400

    aaa

--- ping.cpp
+++ ping.cpp
@@ -9,22 +9,27 @@ int main() //int argc, char** argv
     //std::cout<< "You have entered " << argv[1] << " for all values" << std::endl ;
     const auto model = fdeep::load_model("./fdeep_ping.json");
 
+    bool gameDone = false;
     std::string input_string;
-    std::getline (std::cin, input_string);
+    while (!gameDone) {
+        
+        std::getline (std::cin, input_string);
 
-    std::cout<< "HERE: " << input_string << std::endl;
-    float string_num = std::stod(input_string.substr(0,1));
+        std::cout<< "HERE: " << input_string << std::endl;
+        float string_num = std::stod(input_string.substr(0,1));
 
-    fdeep::tensor5 input_data(fdeep::shape5(1, 1, 1, 420, 6), 0);
+        fdeep::tensor5 input_data(fdeep::shape5(1, 1, 1, 420, 6), 0);
 
-    for (int i = 0; i < 420; i++) {
-        for (int j = 0; j < 6; j++) {
-            input_data.set(0, 0, 0, i, j, string_num);
+        for (int i = 0; i < 420; i++) {
+            for (int j = 0; j < 6; j++) {
+                input_data.set(0, 0, 0, i, j, string_num);
+            }
         }
-    }
 
-    const auto result = model.predict({input_data});
-    std::cerr << "output: " << fdeep::show_tensor5s(result) << std::endl;
+        const auto result = model.predict({input_data});
+        std::cerr << "output: " << fdeep::show_tensor5s(result) << std::endl;
+    }
+    
 
     return 0;
 }
