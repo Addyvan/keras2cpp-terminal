129c251858715a5226571bf07a25d4e4f92d1256
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 11:36:18 -0400

    a

--- ping.cpp
+++ ping.cpp
@@ -6,14 +6,14 @@
 int main()
 {
   const auto model = fdeep::load_model("./fdeep_ping.json");
-  std::vector< std::vector<int> > input_data(420);
+  std::vector< std::vector<float> > input_data(420);
   for (int i = 0; i < 420; i++) {
-      input_data[i] = std::vector<int>(6);
+      input_data[i] = std::vector<float>(6);
       for (int j = 0; j < 6; j++) {
           input_data[i][j] = 3;
       }
   }
-  const fdeep::shared_int_vec sv(fplus::make_shared_ref<fdeep::int_vec>(std::move(input_data)));
+  const fdeep::shared_float_vec sv(fplus::make_shared_ref<fdeep::float_vec>(std::move(input_data)));
   //fdeep::tensor5 input_tensor = fdeep::tensor5(fdeep::shape5(1,1,1,420,6), input_data);
   //const auto result = model.predict({fdeep::tensor5(fdeep::shape5(1, 1, 1, 1, 4), {1, 2, 3, 4})});
   //std::cout << fdeep::show_tensor5s(result) << std::endl;
