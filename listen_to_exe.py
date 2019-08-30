3ca4551dd95e3fae1821c1ac1b1e2ff5c7184eb2
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 12:47:52 -0400

    aaa

--- listen_to_exe.py
+++ listen_to_exe.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import sys
-from subprocess import Popen
-
+from subprocess import Popen, PIPE
+"""
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
@@ -27,7 +27,7 @@ def _read_output(proc):
     while proc.poll() == None:
         out_line = proc.stderr.readline().decode('utf-8').encode('utf-8')
         print("SUH -- ", out_line)
-        """
+        
         if len(out_line) >= 0:
             if out_line.find("output") != - 1:
                 data = out_line[8:]
@@ -36,14 +36,19 @@ def _read_output(proc):
             else:
                 print("NOPE: ", out_line)
             out.append(out_line)
-        """
+        
     return ( out, err )
 
 process_command = "./ping 1"
-proc = Popen(process_command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
+proc = Popen(process_command, shell=True, stdout=sys.stdout, stderr=sys.stderr, stdin=PIPE)
 
 try:
     while proc.returncode == None:
         outs , errs = _read_output(proc)
 except:
     outs, errs = proc.communicate()
+"""
+
+p = Popen("./ping 1", stdout=sys.stdout, stdin=PIPE, stderr=sys.stderr)    
+grep_stdout = p.communicate(input=b'1')[0]
+print("python: ",  grep_stdout.decode())
--- ping.cpp
+++ ping.cpp
@@ -4,17 +4,19 @@
 #include <fdeep/fdeep.hpp>
 #include <stdlib.h>
 
-int main(int argc, char** argv)
+int main() //int argc, char** argv
 {
-    std::cout<< "You have entered " << argv[1] << " for all values" << std::endl ;
-
+    //std::cout<< "You have entered " << argv[1] << " for all values" << std::endl ;
     const auto model = fdeep::load_model("./fdeep_ping.json");
 
+    string input_string;
+    std::getline (std::cin, input_string);
+    
     fdeep::tensor5 input_data(fdeep::shape5(1, 1, 1, 420, 6), 0);
 
     for (int i = 0; i < 420; i++) {
         for (int j = 0; j < 6; j++) {
-            input_data.set(0, 0, 0, i, j, atof(argv[1]));
+            input_data.set(0, 0, 0, i, j, atof(input_string[0]));
         }
     }
 
