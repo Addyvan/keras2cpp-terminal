acc8ab8d85c08698b75dff4aa7efd99c1085c712
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 16:14:08 -0400

    a

--- playground/rl_agent_prod/keras_model_2.cpp
+++ playground/rl_agent_prod/keras_model_2.cpp
@@ -6,11 +6,11 @@
 
 class KerasModel {
     
-    fdeep:: model;
+    fdeep::model model;
     public:
 
         void init(std::string model_path) {
-            model = fdeep::load_model(model_path);
+            this->model = fdeep::load_model(model_path);
         }
 
 
