06e5200e428c384d445dc7b9dd874e37fc6e6f45
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 20:36:08 -0400

    a

--- playground/rl_agent_prod/keras_cpp_model.py
+++ playground/rl_agent_prod/keras_cpp_model.py
@@ -58,7 +58,7 @@ class KerasCPPModel:
         
         self.proc.stdin.write(state_string.encode('utf-8'))
         time.sleep(1)
-        print(self.proc.stderr.decode('utf-8'))
+        print(self.proc.stderr)
         '''
         while self.proc.poll() is None:
             line = self.proc.stderr.readlines()
