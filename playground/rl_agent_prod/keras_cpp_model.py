2ab66d5969568ff9b15992730d235417b924ed67
addyvan <avandenhoeve14@ubishops.ca>
2019-08-30 21:00:28 -0400

    aaa

--- playground/rl_agent_prod/keras_cpp_model.py
+++ playground/rl_agent_prod/keras_cpp_model.py
@@ -70,3 +70,5 @@ if __name__ == "__main__":
     print("making prediction")
     predictions = model.predict(state)
     print("predictions: ", predictions)
+    predictions = model.predict(state)
+    print("predictions: ", predictions)
